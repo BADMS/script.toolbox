fe6d603242cf18a39ef7a76fc620fafa3862df86
BADMS <dan@allscool.co.uk>
2015-11-24 10:17:41 +0000

    all working apart from 1st run check addon_dir

--- default.py
+++ default.py
@@ -20,7 +20,6 @@ class Main:
 
     def __init__(self):
         log("version %s started" % ADDON_VERSION)
-        xbmc.executebuiltin('SetProperty(colorbox_running,True,home)')
         self._init_vars()
         self._parse_argv()
         if self.infos:
@@ -29,16 +28,6 @@ class Main:
             self._selection_dialog()
         if self.control == "plugin":
             xbmcplugin.endOfDirectory(self.handle)
-        xbmc.executebuiltin('ClearProperty(colorbox_running,home)')
-        while self.daemon and not xbmc.abortRequested:
-            self.image_now = xbmc.getInfoLabel("Player.Art(thumb)")
-            if self.image_now != self.image_prev:
-                self.image_prev = self.image_now
-                image, imagecolor = Filter_Image(self.image_now, self.radius)
-                HOME.setProperty(self.prefix + 'ImageFilter', image)
-                HOME.setProperty(self.prefix + "ImageColor", imagecolor)
-            else:
-                xbmc.sleep(300)
 
     def _StartInfoActions(self):
         for info in self.infos:
@@ -49,16 +38,21 @@ class Main:
                 HOME.setProperty(self.prefix + 'ImageFilter', image)
                 HOME.setProperty(self.prefix + "ImageColor", imagecolor)
             elif info == 'pixelate':
-		image, imagecolor = Filter_Pixelate(self.id, self.pixels)
-		log("Pixelate image %s with pixels %i" % (self.id, self.pixels))
-		HOME.setProperty(self.prefix + 'ImageFilter', image)
-		HOME.setProperty(self.prefix + "ImageColor", imagecolor)
-		HOME.setProperty(self.prefix + 'ImageUpdating', '1')
+				image, imagecolor = Filter_Pixelate(self.id, self.pixels)
+				log("Pixelate image %s with pixels %i" % (self.id, self.pixels))
+				HOME.setProperty(self.prefix + 'ImageFilter', image)
+				HOME.setProperty(self.prefix + "ImageColor", imagecolor)
+				HOME.setProperty(self.prefix + 'ImageUpdating', '1')
             elif info == 'twotone':
-		image = Filter_Twotone(self.id, self.black, self.white)
-		log("Twotone image %s with color1 %s color2 %s" % (self.id, self.black, self.white))
-		HOME.setProperty(self.prefix + 'ImageFilter', image)
-		HOME.setProperty(self.prefix + 'ImageUpdating', '1')
+				image = Filter_Twotone(self.id, self.black, self.white)
+				log("Twotone image %s with color1 %s color2 %s" % (self.id, self.black, self.white))
+				HOME.setProperty(self.prefix + 'ImageFilter', image)
+				HOME.setProperty(self.prefix + 'ImageUpdating', '1')
+            elif info == 'posterize':
+				image = Filter_Posterize(self.id, self.bits)
+				log("Posterize image %s with bits %i" % (self.id, self.bits))
+				HOME.setProperty(self.prefix + 'ImageFilter', image)
+				HOME.setProperty(self.prefix + 'ImageUpdating', '1')
 
     def _init_vars(self):
         self.window = xbmcgui.Window(10000)  # Home Window
@@ -68,14 +62,12 @@ class Main:
         self.dbid = ""
         self.prefix = ""
         self.radius = 5
+        self.bits = 8
         self.pixels = 20
-        self.black = "#000000"
-        self.white = "#FFFFFF"
+        self.black = ""
+        self.white = ""
         self.daemon = False
-        self.image_now = ""
-        self.image_prev = ""
         self.autoclose = ""
-        # self.Monitor = colorboxMonitor(self)
 
     def _parse_argv(self):
         args = sys.argv
@@ -98,24 +90,12 @@ class Main:
                 self.radius = int(arg[7:])
             elif arg.startswith('pixels='):
                 self.pixels = int(arg[7:])
+            elif arg.startswith('bits='):
+                self.bits = int(arg[5:])
             elif arg.startswith('black='):
-                self.black = int(arg[6:])
+                self.black = RemoveQuotes(arg[6:])
             elif arg.startswith('white='):
-                self.white = int(arg[6:])
-
-
-class colorboxMonitor(xbmc.Monitor):
-
-    def __init__(self, *args, **kwargs):
-        xbmc.Monitor.__init__(self)
-
-    def onPlayBackStarted(self):
-        pass
-        # HOME.clearProperty(self.prefix + 'ImageFilter')
-        # Notify("test", "test")
-        # image, imagecolor = Filter_Image(self.id, self.radius)
-        # HOME.setProperty(self.prefix + 'ImageFilter', image)
-        # HOME.setProperty(self.prefix + "ImageColor", imagecolor)
+                self.white = RemoveQuotes(arg[6:])
 
 
 if __name__ == "__main__":
